35da7bf386bef67c7ea7f72ea51ff6e75416a2dd
Masaaki <softwaremasaaki@gmail.com>
2023-01-07 13:02:11 +0700

    Confirm with PLC.

--- test_opc/Form1.Designer.vb
+++ test_opc/Form1.Designer.vb
@@ -25,6 +25,7 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
@@ -54,11 +55,21 @@ Partial Class Form1
         Me.Label3.TabIndex = 2
         Me.Label3.Text = "Label3"
         '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(34, 152)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Label4"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(531, 248)
+        Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
@@ -72,4 +83,5 @@ Partial Class Form1
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
 End Class
--- test_opc/Form1.vb
+++ test_opc/Form1.vb
@@ -1,12 +1,17 @@
-﻿Public Class Form1
+﻿Imports System.Threading
+
+Public Class Form1
     Private WithEvents bufferedWriter As NetworkVariableBufferedWriter(Of Int32)
     Private readerInterval As NetworkVariableReader(Of Int32)
     Private readerBoolean As NetworkVariableReader(Of Boolean)
-    Private readerInt As NetworkVariableReader(Of Int64)
+    'Private WithEvents writeInt As NetworkVariableBufferedWriter(Of Int16)
+    Private readerInt As NetworkVariableReader(Of Int16) 'Short read as Int16
+    Private readerStr As NetworkVariableReader(Of UInt32) ' String read as Unit32
     'Private Const NetworkVariableLocation As String = "\\localhost\system\doublearray"
     Private Const DoubleNetworkVariableLocation As String = "\\localhost\OPC\OPC\PLC\_System\_WriteOptimizationDutyCycle"
-    Private Const BooleanExample As String = "\\localhost\OPC\OPC\PLC\PLC0\Test_L_Start"
-    Private Const IntExample As String = "\\localhost\OPC\OPC\PLC\PLC0\Program_PN"
+    Private Const BooleanExample As String = "\\localhost\OPC\OPC\PLC\PLC0\manual"
+    Private Const IntExample As String = "\\localhost\OPC\OPC\PLC\PLC0\Print_RH_NGCode"
+    Private Const StrExample As String = "\\localhost\OPC\OPC\PLC\PLC0\Print_HW_Ver"
 
     Public Sub New()
         InitializeComponent()
@@ -15,15 +20,22 @@
         'doubleBufferedWriter = New NetworkVariableBufferedWriter(Of Double)(DoubleNetworkVariableLocation)
         readerInterval = New NetworkVariableReader(Of Int32)(DoubleNetworkVariableLocation)
         readerBoolean = New NetworkVariableReader(Of Boolean)(BooleanExample)
-        readerInt = New NetworkVariableReader(Of Int64)(IntExample)
+        'writeInt = New NetworkVariableBufferedWriter(Of Int16)(IntExample)
+        readerInt = New NetworkVariableReader(Of Int16)(IntExample)
+        readerStr = New NetworkVariableReader(Of UInt32)(StrExample)
+        'writeInt.Connect()
+        readerInt.Connect()
         bufferedWriter.Connect()
         readerInterval.Connect()
         readerBoolean.Connect()
-        readerInt.Connect()
+        readerStr.Connect()
+        Thread.Sleep(1000)
         'doubleBufferedWriter.Connect()
         Label1.Text = readerInterval.ReadData().GetValue
         Label2.Text = readerBoolean.ReadData().GetValue
         Label3.Text = readerInt.ReadData().GetValue
+        Label4.Text = readerStr.ReadData().GetValue
 
     End Sub
+
 End Class
